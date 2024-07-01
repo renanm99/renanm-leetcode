b664da0c68182577269c272273c50c10c80299a2
Renan Machado <renanoliveira2199@gmail.com>
2024-07-01 19:45:28 -0300

    leetcode 3 - Longest Substring W/out Repeating Characters

---
+++ leetcode3.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	str := "eeydgwdykpv"
+	fmt.Println(lengthOfLongestSubstring(str))
+}
+
+func lengthOfLongestSubstring(s string) int {
+	m := make(map[string]int)
+
+	max := 0
+	substr := ""
+	head := 0
+	for i := 0; i < len(s); i++ {
+		letter := fmt.Sprintf("%c", s[i])
+
+		if m[letter] > 0 {
+			o := m[letter]
+			if head > o {
+				substr = fmt.Sprint(s[head:i]) + letter
+			} else {
+				substr = fmt.Sprint(s[o:i]) + letter
+				head = o
+			}
+			if o == i {
+				substr = letter
+				head = i
+			}
+
+		} else {
+			substr = substr + letter
+		}
+		m[letter] = i + 1
+
+		if len(substr) > max {
+			max = len(substr)
+		}
+	}
+
+	if len(substr) >= len(m) {
+		return len(m)
+	}
+
+	if len(substr) > max {
+		return len(substr)
+	}
+	return max
+}
